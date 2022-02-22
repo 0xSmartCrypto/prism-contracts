7a0fa9723f50989f0d8694a23f4bfc55667ec48b
Andrés Mejía <amejia@jumptrading.com>
2022-02-22 13:47:40 -0600

    Add comment explaining what this crate does.

--- packages/prism_testing/src/lib.rs
+++ packages/prism_testing/src/lib.rs
@@ -1,1 +1,8 @@
+//! This crate provides utilities to write unit tests.
+//!
+//! There's shouldn't be any production code in this crate, since it won't
+//! compile down to Wasm (because it imports CosmWasm libraries with
+//! "#[cfg(not(target_arch = "wasm32"))]" attributes, for example:
+//! https://github.com/CosmWasm/cosmwasm/blob/v0.16.4/packages/std/src/lib.rs#L92-L96)
+
 pub mod mock_querier;
